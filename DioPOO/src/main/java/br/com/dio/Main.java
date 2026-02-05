69edb0938bed389d86eddc61a826afc617cb765b
LeandroMenoci <menoci.leandro1@gmail.com>
2026-02-05 01:47:30 -0300

    Mentoria e Curso

---
+++ DioPOO/src/main/java/br/com/dio/Main.java
@@ -0,0 +1,28 @@
+package br.com.dio;
+
+import br.com.dio.dominio.Curso;
+import br.com.dio.dominio.Mentoria;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso Java");
+        curso1.setDescricao("Descrição do curso Java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso C#");
+        curso2.setDescricao("Descrição do curso C#");
+        curso2.setCargaHoraria(10);
+
+        Mentoria mentoria1 = new Mentoria();
+        mentoria1.setTitulo("Mentoria de Java");
+        mentoria1.setDescricao("Descrição da mentoria de Java");
+        mentoria1.setData(java.time.LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(mentoria1);
+        System.out.println(curso2);
+    }
+}
