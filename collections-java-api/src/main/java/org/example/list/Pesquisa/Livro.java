611c5ca33796c60bdff6f793a650c2065c4c53d1
LeandroMenoci <menoci.leandro1@gmail.com>
2026-02-12 09:56:07 -0300

    Pesquisa em List

---
+++ collections-java-api/src/main/java/org/example/list/Pesquisa/Livro.java
@@ -0,0 +1,40 @@
+package org.example.list.Pesquisa;
+
+public class Livro {
+
+    //atributos
+    private String titulo;
+    private String autor;
+    private int anoPublicacao;
+
+    //construtor
+
+    public Livro(String titulo, String autor, int anoPublicacao) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.anoPublicacao = anoPublicacao;
+    }
+
+    //metodos
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public int getAnoPublicacao() {
+        return anoPublicacao;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "titulo='" + titulo + '\'' +
+                ", autor='" + autor + '\'' +
+                ", anoPublicacao=" + anoPublicacao +
+                '}';
+    }
+}
